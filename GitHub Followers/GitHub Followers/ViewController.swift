6c166d64f54dde482ec05f38923b1e28ab8d2dac
Jason Pinlac <jasonpinlacdev@gmail.com>
2020-04-21 17:49:16 -0400

    start of the github followers project

---
+++ GitHub Followers/GitHub Followers/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitHub Followers
+//
+//  Created by Jason Pinlac on 4/21/20.
+//  Copyright © 2020 Jason Pinlac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.systemPink
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+
+    
+
+}
+
