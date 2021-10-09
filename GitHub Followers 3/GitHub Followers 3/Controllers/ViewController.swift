cf4c835f8cd3acb4411a4fa86653b841d539efac
Jason Pinlac <jasonpinlacdev@gmail.com>
2021-10-09 18:17:39 -0400

    GitHub Followers refresh. Setup for the appDelegate and SceneDelegate. Programmatic Approach without Storyboard.

---
+++ GitHub Followers 3/GitHub Followers 3/Controllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GitHub Followers 3
+//
+//  Created by Jason Pinlac on 10/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+
+}
+
